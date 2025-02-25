5afab76e11550e352dc2e48e8708d8a7418ab67a
Selvakumar Boominathan <selvans2k@gmail.com>
2025-02-26 00:54:08 +0530

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchvalue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
   return (
     <div classname="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MivieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+
       <div className="row">
         <MovieList movies={movies} />
       </div>
